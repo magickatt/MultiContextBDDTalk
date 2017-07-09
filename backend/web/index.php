0b1d23ad39702c7718e427c537ce2ec80f373d84
Andrew Kirkpatrick <a.kirkpatrick@outlook.com>
2017-07-09 17:19:50 -0400

    Back-end container now working with PHP-FPM and Nginx

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,3 +1,14 @@
 <?php
 
-phpinfo();
+// Register the autoloader (which in turns loads everything else)
+require_once __DIR__.'/../vendor/autoload.php';
+
+// Create a Silex framework application
+$application = new Silex\Application();
+
+$application->get('/', function() use($application) {
+    return 'Hello World';
+});
+
+// Run the application (handle the request, return a response, etc.)
+$application->run();
