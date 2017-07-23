945610b467980f0222feef7d9f1ab9672114c3fc
Andrew Kirkpatrick <a.kirkpatrick@outlook.com>
2017-07-22 23:36:07 -0400

    Adding Behat tests to CI

--- bdd_tests.sh
+++ bdd_tests.sh
@@ -6,9 +6,6 @@ CONTAINER_NAME=bddtalk_backend.1.$(docker service ps -f 'name=bddtalk_backend.1'
 # Run the unit tests and integration tests inside the back-end container
 docker exec -i $CONTAINER_NAME bash <<'DOCKER_EXEC_COMMAND'
 cd /var/backend
-echo -e "Running unit tests..."
-bin/phpspec run
-echo -e "\nRunning integration tests...\n"
-bin/phpunit
+bin/behat
 exit
 DOCKER_EXEC_COMMAND
