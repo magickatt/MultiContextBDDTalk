3e50a48585dbaeaf58095d981b51463183424941
Andrew Kirkpatrick <a.kirkpatrick@outlook.com>
2017-07-23 17:43:26 -0400

    Running behavioural tests in separate container

--- bdd-tests.sh
+++ bdd-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Determine which container in the stack is the back-end container
-CONTAINER_NAME=bddtalk_backend.1.$(docker service ps -f 'name=bddtalk_backend.1' bddtalk_backend -q --no-trunc)
+# Determine which container in the stack is the test runner container
+CONTAINER_NAME=bddtalk_testrunner.1.$(docker service ps -f 'name=bddtalk_testrunner.1' bddtalk_testrunner -q --no-trunc)
 
-# Run the unit tests and integration tests inside the back-end container
+# Run the behavioural tests inside the test runner container
 docker exec -i $CONTAINER_NAME bash <<'DOCKER_EXEC_COMMAND'
 cd /var/backend
 bin/behat
